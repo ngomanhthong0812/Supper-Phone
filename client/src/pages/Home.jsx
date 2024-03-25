4179bdfeff9bf502eb49047c1875c72e308f2570
quangk22 <quangcute504@gmail.com>
2024-03-25 21:04:23 +0700

    add font and icons

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import { CiBoxList } from "react-icons/ci";
 export default function Home() {
   return (
     <div>
-     <h1>Hello World</h1>
+     <h1 className='bg-slate-500'>Hello World <CiBoxList /></h1>
     </div>
   )
 }
