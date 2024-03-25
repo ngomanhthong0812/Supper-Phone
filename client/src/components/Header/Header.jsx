049c2e2d065950ea26acba1e3c71de682d80d6b4
Ngo Manh Thong <ngomanhthong0812>
2024-03-25 21:08:39 +0700

    thong

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import { CiSearch } from "react-icons/ci";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FiPhoneCall } from "react-icons/fi";
+import { FaUser } from "react-icons/fa6";
+import { BsCart3 } from "react-icons/bs";
+import { IoIosArrowDown } from "react-icons/io";
+
+import logo from '../../asset/images/logo.png'
+
 export default function Header() {
   return (
     <div>
-      
+      <img src={logo} alt="logo" />
+      <div><input type="text" /><CiSearch /></div>
+      <div><FaMapMarkerAlt /><span>Hệ thống cửa hàng <p>7 cửa hàng</p></span></div>
+      <div><FiPhoneCall /><span>Gọi mua hàng <p>1900 2318</p></span></div>
+      <div><FaUser /><span>Thông tin <p>Tài khoản <IoIosArrowDown /></p></span></div>
+      <div><BsCart3 /><span>Giỏ hàng</span></div>
     </div>
   )
 }
