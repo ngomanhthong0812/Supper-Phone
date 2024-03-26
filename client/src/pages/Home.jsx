6f6cce04a1733ae361d71ffd366d7c3067fa76c6
ngomanhthong0812 <139620427+ngomanhthong0812@users.noreply.github.com>
2024-03-26 22:49:19 +0700

    thong

--- client/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { CiBoxList } from "react-icons/ci";
-export default function Home() {
-  return (
-    <div>
-     <h1 className='bg-slate-500'>Hello World <CiBoxList /></h1>
-    </div>
-  )
-}
--- client/src/pages/TrangChu.jsx
+++ client/src/pages/TrangChu.jsx
@@ -3,12 +3,14 @@ import { CiBoxList } from "react-icons/ci";
 
 import Header from '../components/Header/Header';
 import Navbar from '../components/Navbar/Navbar';
+import Footer from '../components/Footer/Footer';
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Navbar />
+      <Footer/>
     </div>
   )
 }
