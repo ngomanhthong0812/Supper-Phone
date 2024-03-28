dad231f5b7a1ac8d53b981ef32ad72447c6c8f21
NGUYENDANG19 <dangnguyen19092001@gmail.com>
2024-03-28 08:45:14 +0700

    up

--- client/src/pages/TrangChu.jsx
+++ client/src/pages/TrangChu.jsx
@@ -7,6 +7,9 @@ import Footer from "../components/Footer/Footer";
 import Banner from "../components/Banner/Banner";
 import ProductsList from "../components/ProductsList/ProductsList";
 import Customers from "../components/Customers/Customers";
+import Post from "../components/Post/Post";
+
+
 export default function Home() {
   return (
     <div>
@@ -14,6 +17,7 @@ export default function Home() {
       <Navbar />
       <Banner />
       <ProductsList />
+      <Post />
       <Customers/>
       <Footer />
     </div>
